4a741ab24ade93a47bdd3ec51e15c6fe3f0f71bc
Wieger IJntema <wieger.ijntema@gmail.com>
2016-06-07 13:57:14 +0200

    added constructor

--- include/playablenote.h
+++ include/playablenote.h
@@ -8,6 +8,13 @@ public :
 	int octave;
 	double duration;
 	int bar_location;
+
+  PlayableNote(int note_ID_, int octave_, double duration_, int bar_location_) :
+    note_ID(note_ID_),
+    octave(octave_),
+    duration(duration_),
+    bar_location(bar_location_)
+  {}
 };
 
 #endif
