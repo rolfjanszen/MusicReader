8a52552356de613ebd72c1bcf30920a00c2dba1d
Wieger IJntema <wieger.ijntema@gmail.com>
2016-06-07 09:14:20 +0200

    Added SimplePlayer

---
+++ SimplePlayer/SimplePlayer.cpp
@@ -0,0 +1,6 @@
+#include "SimplePlayer.h"
+
+SimplePlayer::SimplePlayer()
+{
+
+}
---
+++ SimplePlayer/SimplePlayer.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLEPLAYER_H
+#define SIMPLEPLAYER_H
+
+
+class SimplePlayer
+{
+public:
+  SimplePlayer();
+};
+
+#endif // SIMPLEPLAYER_H
